8fd80a26f81904782b2517e64a15703560c4981b
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2025-03-05 12:27:57 -0500

    Modifications minieurs

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   PORT: process.env.PORT || 3100,
   API_IR_TRACKER: 'http://localhost:3100',
-  DSPACE_API_URL: 'https://papyrus-dev.bib.umontreal.ca/server/api',
+  DSPACE_API_URL: 'http://localhost:8080/server/api',
   DSPACE_USERNAME: 'natalia.jabinschi@umontreal.ca',
-  DSPACE_PASSWORD: 'udem2021!'
+  DSPACE_PASSWORD: 'dspace'
 };
 
 module.exports = config;
